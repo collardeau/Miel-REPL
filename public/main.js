d2e24a3e84e04bc0b3320da832d2842865650bf0
Thomas Collardeau <thomas@collardeau.com>
2015-07-22 22:31:39 +0200

    update miel dependency

--- public/main.js
+++ public/main.js
@@ -133,7 +133,7 @@
 /* 5 */
 /***/ function(module, exports) {
 
-	var REGEX = new RegExp('^([ \\t]*)([_$a-zA-Z\xA0-\uFFFF]+ *):=', 'gm');
+	var REGEX = new RegExp('^([ \\t]*)([_$a-zA-Z0-9\xA0-\uFFFF]+ *):=', 'gm');
 
 	module.exports = function(input){
 	  return input.replace(REGEX, '$1var $2=');
